f200b1e7c3ec490c8efbdc18baa0671b9c7a25c3
Stephen Trewick <stephent818@gmail.com>
2019-04-12 13:34:54 -0700

    add prototypes for checking size and toppings and adding prices

--- js/scripts.js
+++ js/scripts.js
@@ -15,20 +15,49 @@ Order.prototype.addPizza = function(pizza) {
 }
 
 //Business Logic for pizzas
-function Pizza (size, toppings) {
+function Pizza (size, toppings, price) {
   this.size = size,
-  this.toppings = []
+  this.toppings = [],
+  this.price = price;
+}
+
+Pizza.prototype.checkSize = function() {
+  if(this.size === "sm") {
+    this.price += 9.99;
+  } else if (this.size === "md") {
+    this.price += 14.99;
+  } else if (this.size === "lg") {
+    this.price += 18.99;
+  } else if (this.size === "xl") {
+    this.price += 21.99;
+  }
+}
+
+Pizza.prototype.checkToppings = function() {
+  if (this.toppings.length === 0) {
+    this.price = this.price;
+  } else if (this.toppings.length <= 2) {
+    this.price += 1.5;
+  } else if (this.toppings.length <= 5) {
+    this.price += 3;
+  } else {
+    this.price += 4.5;
+  }
 }
 
 //User Interface Logic
 var order = new Order();
 
+
+
 $(document).ready(function() {
   $("form#new-pizza").submit(function(event) {
     event.preventDefault();
     var inputtedSize = $("size").val();
-    var inputtedToppings = $("");
-    var newPizza = new Pizza(inputtedSize, inputtedToppings);
+    var inputtedToppings = $("input:radio[name=topping]:checked").val();
+    var newPizza = new Pizza(inputtedSize, inputtedToppings, price);
     order.addPizza(newPizza);
+    newPizza.checkSize();
+    newPizza.checkToppings();
   });
 });
